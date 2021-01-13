7e83ead9b0d52e06f9ab7741c91a7d7f938a74a2
dgrammatiko <d.grammatiko@gmail.com>
2021-01-13 14:30:46 +0100

    Meh modals need a public contructor

--- dist/js/modal.es6.js
+++ dist/js/modal.es6.js
@@ -631,88 +631,90 @@ if (window.Joomla) {
   window.Joomla.Bootstrap.Methods.Modal = Modal;
   window.Joomla.Bootstrap.Instances.Modal = new WeakMap();
 
+  window.Joomla.Bootstrap.Methods.initModal = (modal) => {
+    window.Joomla.Bootstrap.Instances.Modal.set(modal, new window.Joomla.Bootstrap.Methods.Modal(modal));
+
+    // Comply with the Joomla API - Bound element.open/close
+    modal.open = () => { window.Joomla.Bootstrap.Instances.Modal.get(modal).show(modal); };
+    modal.close = () => { window.Joomla.Bootstrap.Instances.Modal.get(modal).hide(); };
+
+    // Do some Joomla specific changes
+    modal.addEventListener('show.bs.modal', () => {
+      // Comply with the Joomla API - Set the current Modal ID
+      Joomla.Modal.setCurrent(modal);
+
+      if (modal.dataset.url) {
+        const modalBody = modal.querySelector('.modal-body');
+        const iframe = modalBody.querySelector('iframe');
+
+        if (iframe) {
+          iframe.parentNode.removeChild(iframe);
+        }
+
+        // @todo merge https://github.com/joomla/joomla-cms/pull/20788
+        // Hacks because com_associations and field modals use pure javascript in the url!
+        if (modal.dataset.iframe.indexOf("document.getElementById") > 0){
+          const iframeTextArr = modal.dataset.iframe.split('+');
+          let idFieldArr = iframeTextArr[1].split('"');
+          let el;
+
+          idFieldArr[0] = idFieldArr[0].replace(/&quot;/g,'"');
+
+          if (!document.getElementById(idFieldArr[1])) {
+            el = eval(idFieldArr[0]); // This is UNSAFE!!!!
+          } else {
+            el = document.getElementById(idFieldArr[1]).value;
+          }
+
+          modalBody.insertAdjacentHTML('afterbegin', `${iframeTextArr[0]}${el}${iframeTextArr[2]}`);
+        } else {
+          modalBody.insertAdjacentHTML('afterbegin', modal.dataset.iframe);
+        }
+      }
+    });
+
+    modal.addEventListener('shown.bs.modal', () => {
+      const modalBody = modal.querySelector('.modal-body');
+      const modalHeader = modal.querySelector('.modal-header');
+      const modalFooter = modal.querySelector('.modal-footer');
+      // const modalRects = modal.getBoundingClientRect();
+      const modalHeaderRects = modalHeader.getBoundingClientRect();
+      // const modalHeight = modalRects.height;
+      const modalHeaderHeight = modalHeaderRects.height;
+      const modalBodyHeightOuter = modalBody.offsetHeight;
+      const modalBodyHeight = parseFloat(getComputedStyle(modalBody, null).height.replace("px", ""));
+      const modalFooterHeight = parseFloat(getComputedStyle(modalFooter, null).height.replace("px", ""));
+      const padding = modalBody.offsetTop;
+      const maxModalHeight = parseFloat(getComputedStyle(document.body, null).height.replace("px", ""))-(padding * 2);
+      const modalBodyPadding = modalBodyHeightOuter - modalBodyHeight;
+      const maxModalBodyHeight = maxModalHeight - (modalHeaderHeight + modalFooterHeight + modalBodyPadding);
+
+      if (modal.dataset.url) {
+        const iframeEl = modal.querySelector('.iframe');
+        var iframeHeight = parseFloat(getComputedStyle(iframeEl, null).height.replace("px", ""));
+        if (iframeHeight > maxModalBodyHeight){
+          modalBody.style.maxHeight = maxModalBodyHeight;
+          modalBody.style.overflowY = 'auto';
+          iframeEl.style.maxHeight = maxModalBodyHeight - modalBodyPadding;
+        }
+      }
+    });
+
+    modal.addEventListener('hide.bs.modal', () => {
+      const modalBody = modal.querySelector('.modal-body');
+      modalBody.style.maxHeight = 'initial';
+    });
+
+    modal.addEventListener('hidden.bs.modal', () => {
+      // Comply with the Joomla API - Remove the current Modal ID
+      Joomla.Modal.setCurrent('');
+    });
+  };
+
   const modals = [].slice.call(document.querySelectorAll('.joomla-modal'));
 
   if (modals.length) {
-    modals.forEach((modal) => {
-      window.Joomla.Bootstrap.Instances.Modal.set(modal, new window.Joomla.Bootstrap.Methods.Modal(modal));
-
-      // Comply with the Joomla API - Bound element.open/close
-      modal.open = () => { window.Joomla.Bootstrap.Instances.Modal.get(modal).show(modal); };
-      modal.close = () => { window.Joomla.Bootstrap.Instances.Modal.get(modal).hide(); };
-
-      // Do some Joomla specific changes
-      modal.addEventListener('show.bs.modal', () => {
-        // Comply with the Joomla API - Set the current Modal ID
-        Joomla.Modal.setCurrent(modal);
-
-        if (modal.dataset.url) {
-          const modalBody = modal.querySelector('.modal-body');
-          const iframe = modalBody.querySelector('iframe');
-
-          if (iframe) {
-            iframe.parentNode.removeChild(iframe);
-          }
-
-          // @todo merge https://github.com/joomla/joomla-cms/pull/20788
-          // Hacks because com_associations and field modals use pure javascript in the url!
-          if (modal.dataset.iframe.indexOf("document.getElementById") > 0){
-            const iframeTextArr = modal.dataset.iframe.split('+');
-            let idFieldArr = iframeTextArr[1].split('"');
-            let el;
-
-            idFieldArr[0] = idFieldArr[0].replace(/&quot;/g,'"');
-
-            if (!document.getElementById(idFieldArr[1])) {
-              el = eval(idFieldArr[0]); // This is UNSAFE!!!!
-            } else {
-              el = document.getElementById(idFieldArr[1]).value;
-            }
-
-            modalBody.insertAdjacentHTML('afterbegin', `${iframeTextArr[0]}${el}${iframeTextArr[2]}`);
-          } else {
-            modalBody.insertAdjacentHTML('afterbegin', modal.dataset.iframe);
-          }
-        }
-      });
-
-      modal.addEventListener('shown.bs.modal', () => {
-        const modalBody = modal.querySelector('.modal-body');
-        const modalHeader = modal.querySelector('.modal-header');
-        const modalFooter = modal.querySelector('.modal-footer');
-        // const modalRects = modal.getBoundingClientRect();
-        const modalHeaderRects = modalHeader.getBoundingClientRect();
-        // const modalHeight = modalRects.height;
-        const modalHeaderHeight = modalHeaderRects.height;
-        const modalBodyHeightOuter = modalBody.offsetHeight;
-        const modalBodyHeight = parseFloat(getComputedStyle(modalBody, null).height.replace("px", ""));
-        const modalFooterHeight = parseFloat(getComputedStyle(modalFooter, null).height.replace("px", ""));
-        const padding = modalBody.offsetTop;
-        const maxModalHeight = parseFloat(getComputedStyle(document.body, null).height.replace("px", ""))-(padding * 2);
-        const modalBodyPadding = modalBodyHeightOuter - modalBodyHeight;
-        const maxModalBodyHeight = maxModalHeight - (modalHeaderHeight + modalFooterHeight + modalBodyPadding);
-
-        if (modal.dataset.url) {
-          const iframeEl = modal.querySelector('.iframe');
-          var iframeHeight = parseFloat(getComputedStyle(iframeEl, null).height.replace("px", ""));
-          if (iframeHeight > maxModalBodyHeight){
-            modalBody.style.maxHeight = maxModalBodyHeight;
-            modalBody.style.overflowY = 'auto';
-            iframeEl.style.maxHeight = maxModalBodyHeight - modalBodyPadding;
-          }
-        }
-      });
-
-      modal.addEventListener('hide.bs.modal', () => {
-        const modalBody = modal.querySelector('.modal-body');
-        modalBody.style.maxHeight = 'initial';
-      });
-
-      modal.addEventListener('hidden.bs.modal', () => {
-        // Comply with the Joomla API - Remove the current Modal ID
-        Joomla.Modal.setCurrent('');
-      });
-    });
+    modals.map((modal) => window.Joomla.Bootstrap.Methods.initModal(modal));
   }
 }
 
