200b381420b49a9b9c4318555edde61e0ccc64ea
dgrammatiko <d.grammatiko@gmail.com>
2021-01-18 17:50:20 +0100

    Combine the 2 build tools

--- build.js
+++ build.js
@@ -1,10 +1,13 @@
-const rollup = require('rollup')
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const replace = require('@rollup/plugin-replace');
 const { readdir, readFile, rename, writeFile, rm } = require('fs').promises;
 const {resolve} = require('path');
 const {minify} = require('terser');
 const rimraf = require('rimraf');
+const rollup = require('rollup')
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const replace = require('@rollup/plugin-replace');
+const { babel } = require('@rollup/plugin-babel');
+
+
 const tasks = [];
 
 const createMinified = async (file) => {
@@ -63,6 +66,49 @@ const build = async _ => {
   });
 }
 
+const buildLegacy = async _ => {
+  console.log(`Building Legacy...`);
+
+  const bundle = await rollup.rollup({
+    input: 'src/js/index.es6.js',
+    plugins: [
+      nodeResolve(),
+      replace({
+        'process.env.NODE_ENV': '\'production\''
+      }),
+      babel({
+        exclude: 'node_modules/core-js/**',
+        babelHelpers: 'bundled',
+        babelrc: false,
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              corejs: '3.8',
+              useBuiltIns: 'usage',
+              targets: {
+                "chrome": "58",
+                "ie": "11"
+              },
+              loose: true,
+              bugfixes: true,
+              modules: false
+            }
+          ]
+        ]
+      })
+    ],
+    external: [],
+  })
+
+  await bundle.write({
+    format: 'iife',
+    sourcemap: false,
+    name: 'Bootstrap',
+    file: 'dist/js/bootstrap.es5.js',
+  })
+}
+
 (async () => {
   rimraf.sync('dist');
 
@@ -82,4 +128,15 @@ const build = async _ => {
 
   await Promise.all(tasks).catch(er => console.log(er));
   console.log(`ES6 components ready ✅`)
+
+  try {
+    await buildLegacy('src/js/index.es6.js');
+    const es5File = await readFile('dist/js/bootstrap.es5.js', {encoding: 'utf8'});
+    const mini = await minify(es5File);
+    await writeFile('dist/js/bootstrap.es5.min.js', mini.code, {encoding: 'utf8'});
+    console.log(`Legacy done! ✅`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 })();
